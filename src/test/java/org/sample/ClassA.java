88f4965dabe4acea6b3ae8a2997dcd27eaaed5e0
Gowtham <rajgowtham349@gmail.com>
2022-11-07 19:50:35 +0530

    First push sent

---
+++ src/test/java/org/sample/ClassA.java
@@ -0,0 +1,32 @@
+package org.sample;
+
+public class ClassA {
+	
+	public void method1() {
+	    System.out.println("America");
+		}
+
+
+	  public void method2() {
+	    System.out.println("mexico");
+		}
+
+	  public void method3() {
+	    System.out.println("Turkey");
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
