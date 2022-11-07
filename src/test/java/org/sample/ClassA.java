5f44a8b01615fab57c9a5f63b5e47a9695b808d9
John <john@gmail.com>
2022-11-07 21:15:06 +0530

    Second push

--- src/test/java/org/sample/ClassA.java
+++ src/test/java/org/sample/ClassA.java
@@ -14,6 +14,20 @@ public class ClassA {
 	  public void method3() {
 	    System.out.println("Turkey");
 		}
+	  
+	  public void method4() {
+		    System.out.println("tamilnadu");
+			}
+
+
+		  public void method5() {
+		    System.out.println("karnataka");
+			}
+
+		  public void method6() {
+		    System.out.println("andhra");
+			}
+	  
 }
 
 
